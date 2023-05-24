62d4f0092319e4e3568213203961ad4a23ccfdb7
Mbida Stephan <mbida@medicasure.com>
2023-05-24 18:49:21 +0100

    migration de laravel de la version 6 a la version 7

---
+++ config/export.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'crawl' => true,
+    'paths' => [
+        '/panier',
+        '/boutique',
+        '/login',
+        '/register',
+    ],
+    'include_files' => [
+        'public' => '',
+    ],
+];
